2f1a7b368fd57d698e74c6b21251b7ec0350d4d7
Viliam Valent <viliam@valent.email>
2023-09-01 13:26:54 +0200

    Fix: use CWD for dotenv file discovery

--- nocto/env.py
+++ nocto/env.py
@@ -3,7 +3,7 @@ from functools import cache
 import os
 from pathlib import Path
 
-from dotenv import dotenv_values
+from dotenv import dotenv_values, find_dotenv
 
 from nocto.types import VariableOverrides
 
@@ -12,7 +12,10 @@ from nocto.types import VariableOverrides
 def _env(dotenv: bool, dotenv_file: Path | None, variable_overrides: VariableOverrides) -> Mapping[str, str | None]:
     if not dotenv:
         return os.environ | dict(variable_overrides)
-    return os.environ | dotenv_values(dotenv_file) | dict(variable_overrides)
+    # Use cwd instead of this file's location
+    # Also, help mypy understand the type
+    dotenv_path: str | Path | None = dotenv_file if dotenv_file is not None else find_dotenv(usecwd=True)
+    return os.environ | dotenv_values(dotenv_path) | dict(variable_overrides)
 
 
 def has_env_variable(name: str, dotenv: bool, dotenv_file: Path | None, variable_overrides: VariableOverrides) -> bool:
